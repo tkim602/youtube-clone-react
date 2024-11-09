5590f8fc0b87b04be6300cd3f41808f261c6005d
tkim01 <thkim210414@gmail.com>
2024-11-09 04:30:26 -0500

    add assets and Navbar feature v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Page/Home/Home";
+import Video from "./Pages/Video/Video";
 
-function App() {
+const App = () => {
+  const [sidebar, setSidebar] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar setSidebar={setSidebar} />
+      <Routes>
+        <Route path ="/" element={<Home sidebar={sidebar} />}  />
+        <Route path ="/video/:categoryId/:videoId" element={<Video />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
-export default App;
